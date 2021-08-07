55c9fd7c237c7e138823b9edfaff297707e2746e
Anil Kumar <akchavali9@gmail.com>
2021-08-07 22:43:30 +0200

    debugged

--- linux_diagnostics/health_check.py
+++ linux_diagnostics/health_check.py
@@ -2,7 +2,7 @@
 
 import psutil
 import shutil
-from network import *
+
 
 def check_disk_usage(disk):
     du  = shutil.disk_usage(disk)
@@ -13,5 +13,5 @@ def check_cpu_usage():
     usage = psutil.cpu_percent(1)
     return usage 
 
-if not check_disk_usage("/") > 20 or not check_cpu_usage() < 75:
-    print(f"[ERROR] Disk usage:{check_disk_usage("/"), Cpu usage: {check_cpu_usage()}}")
+if not (check_disk_usage("/") > 20) or not (check_cpu_usage() < 75):
+    print("[ERROR] Disk usage:{check_disk_usage("/")}, Cpu usage: {check_cpu_usage()}")
