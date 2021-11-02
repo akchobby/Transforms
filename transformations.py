959a75fb5f5dbf27436cfa271212503949bd9220
Anil Kumar Chavali <anil-kumar.chavali@flandersmake.be>
2021-11-02 16:54:12 +0100

    not normalizing points

--- transformations.py
+++ transformations.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math 
-import cv2
+#import cv2
 
 
 
 class Quaternion:
 
-    def __init__(self, data, deg=False, rvec=False):
+    def __init__(self, data, deg=False, rvec=False, point=True):
         """
         Function takes a list which has 
         - quaternion values in the order x,y,z,w
@@ -34,7 +34,7 @@ class Quaternion:
 
             elif len(data) == 3:
                 if deg:
-                    data = map(self.deg2rad, data)
+                    data = list(map(self.deg2rad, data))
                 cy = math.cos(data[0] * 0.5)
                 sy = math.sin(data[0] * 0.5)
                 cp = math.cos(data[1] * 0.5)
@@ -49,7 +49,7 @@ class Quaternion:
         elif data.shape[0] == 3 and data.shape[1] == 3:
             tr = np.trace(data)
             if (tr > 0):
-                S = math.sqrt(tr+1.0) * 2; 
+                S = math.sqrt(tr+1.0) * 2
                 self.w = 0.25 * S
                 self.x = (data[2][1] - data[1][2]) / S
                 self.y = (data[0][2] - data[2][0]) / S
@@ -78,8 +78,11 @@ class Quaternion:
         else: 
             print("[ERROR] invalid arguments")
 
+        self.point = point
         self.magnitude = math.sqrt(self.x**2 + self.y**2 + self.z**2 + self.w**2)
-    
+        if not np.isclose(self.magnitude, 1.0, rtol=0) and not point:
+            self.__dict__.update(self.normalize().__dict__)
+
     def __add__(self, q):
         return Quaternion(self.x+q.x,self.y+q.y, self.z+q.z, self.w+q.w)
     
@@ -94,7 +97,7 @@ class Quaternion:
         y = -self.x * q.z + self.y * q.w + self.z * q.x + self.w * q.y
         z =  self.x * q.y - self.y * q.x + self.z * q.w + self.w * q.z
         w = -self.x * q.x - self.y * q.y - self.z * q.z + self.w * q.w
-        return Quaternion([x,y,z,w])
+        return Quaternion([x,y,z,w], point=q.point)
 
     def divide(self, scalar):
         assert not isinstance(scalar, Quaternion), "can only divide scalars"
@@ -119,6 +122,8 @@ class Quaternion:
         """
         Returns ZXY transform but as a list in  x,y,z order. In rad unless deg param is changed 
         """
+
+        #ZYX - surrent case if xyz needed then : z = x, x=z
         # roll (x-axis rotation)
         sinr_cosp = 2 * (self.w * self.x + self.y * self.z)
         cosr_cosp = 1 - 2 * (self.x * self.x + self.y * self.y)
@@ -127,7 +132,7 @@ class Quaternion:
         # pitch (y-axis rotation)
         sinp = 2 * (self.w * self.y - self.z * self.x)
         if (math.fabs(sinp) >= 1):
-            pitch = math.copysign(np.pi / 2, sinp); # use 90 degrees if out of range
+            pitch = math.copysign(np.pi / 2, sinp) # use 90 degrees if out of range
         else:
             pitch = math.asin(sinp)
 
@@ -136,16 +141,44 @@ class Quaternion:
         cosy_cosp = 1 - 2 * (self.y * self.y + self.z * self.z)
         yaw = math.atan2(siny_cosp, cosy_cosp)
 
-        return [roll, pitch, yaw] if not deg else map(self.rad2deg,[roll, pitch, yaw])
+        return [roll, pitch, yaw] if not deg else list(map(self.rad2deg,[roll, pitch, yaw]))
+
     
     def to_rotation_matrix(self):
-        pass
+        sqw = self.w**2
+        sqx = self.x**2
+        sqy = self.y**2
+        sqz = self.z**2
+
+        # invs (inverse square length) is only required if quaternion is not already normalised
+        invs = 1 / (sqx + sqy + sqz + sqw)
+        m00 = ( sqx - sqy - sqz + sqw)*invs #since sqw + sqx + sqy + sqz =1/invs*invs
+        m11 = (-sqx + sqy - sqz + sqw)*invs
+        m22 = (-sqx - sqy + sqz + sqw)*invs
+
+        tmp1 = self.x*self.y
+        tmp2 = self.z*self.w
+        m10 = 2.0 * (tmp1 + tmp2)*invs
+        m01 = 2.0 * (tmp1 - tmp2)*invs
+
+        tmp1 = self.x*self.z
+        tmp2 = self.y*self.w
+        m20 = 2.0 * (tmp1 - tmp2)*invs
+        m02 = 2.0 * (tmp1 + tmp2)*invs
+        tmp1 = self.y*self.z
+        tmp2 = self.x*self.w
+        m21 = 2.0 * (tmp1 + tmp2)*invs
+        m12 = 2.0 * (tmp1 - tmp2)*invs
+
+        return np.array([[m00,m01,m02],
+                        [m10,m11,m12],
+                        [m20,m21,m22]])
     
     def to_list(self):
         return [self.x, self.y, self.z, self.w]
     
     def rotate(self,point):
-        q = Quaternion(point+[0])
+        q = Quaternion(point+[0], point=True)
         return (self * q) * self.conjugate()
     
     def to_rvec(self):
@@ -181,9 +214,9 @@ def plot_3d(ax, rotation=None, translation=[0.0,0.50,0]):
         q = rotation
         p4 = np.array(q.rotate(translation).to_list()[:3])
 
-        p1 = np.array(q.rotate(p1).to_list()[:3]) + p4
-        p2 = np.array(q.rotate(p2).to_list()[:3]) + p4
-        p3 = np.array(q.rotate([p3]).to_list()[:3]) + p4
+        p1 = np.array(q.rotate([1,0,0]).to_list()[:3]) + p4
+        p2 = np.array(q.rotate([0,1,0]).to_list()[:3]) + p4
+        p3 = np.array(q.rotate([0,0,1]).to_list()[:3]) + p4
 
         ax.plot([p4[0],p1[0]],[p4[1],p1[1]],[p4[2],p1[2]], color="r")
         ax.plot([p4[0],p2[0]],[p4[1],p2[1]],[p4[2],p2[2]], color="g")
@@ -209,18 +242,42 @@ def plot_3d(ax, rotation=None, translation=[0.0,0.50,0]):
 
 
 
+class Rotations:
+    def __init__(self, angles, deg=True, order="xyz", extrinsic=True):
+        if deg:
+            angles = list(map(np.deg2rad, angles))
+        if order == "xyz":
+            self.rotation_matrix = np.matmul(np.matmul(self.Rz(angles[2]),self.Ry(angles[1])),self.Rx(angles[0])) if extrinsic else np.matmul(np.matmul(self.Rx(angles[0]), self.Ry(angles[1])),self.Rz(angles[2]))
+        elif order == "zyx":
+            self.rotation_matrix = np.matmul(np.matmul(self.Rx(angles[2]),self.Ry(angles[1])),self.Rz(angles[0])) if extrinsic else np.matmul(np.matmul(self.Rz(angles[0]),self.Ry(angles[1])),self.Rx(angles[2]))
+        else:
+            print("Wrong order, provide in using 'xyz'")
 
+    def Rx(self,phi):
+        return np.array([[1, 0, 0],
+                     [0, np.cos(phi), -np.sin(phi)],
+                     [0, np.sin(phi), np.cos(phi)]])
+
+    def Ry(self,theta):
+        return np.array([[np.cos(theta), 0, np.sin(theta)],
+                         [0, 1, 0],
+                         [-np.sin(theta), 0, np.cos(theta)]])
+
+    def Rz(self, psi):
+        return np.array([[np.cos(psi), -np.sin(psi), 0],
+                 [np.sin(psi), np.cos(psi), 0],
+                 [0, 0, 1]])
 
     
 if __name__=="__main__" :
     
     rotMat = np.array([[0,-1,0],[0,0,-1],[1,0,0]], dtype=np.float64)
-    rotMat_cam0 = np.array([[-0.9995930750515504, 0.028180516203601146, -0.004420725673603042 ],
-                            [0.00408373330785272, -0.012004702762037862, -0.9999196018850028 ],
-                            [-0.028231320040922887, -0.9995307627172317, 0.01188473603633216 ]], dtype=np.float64)
+    rotMat_cam0 = np.array([[ 6.71487041e-01,  7.41014515e-01,  1.62525572e-03],
+ [-7.41009773e-01,  6.71488718e-01, -2.72375351e-03],
+ [-3.10968177e-03,  6.24634817e-04,  9.99994970e-01]], dtype=np.float64)
 
 
-    t_cam0 = [0.06508125947885363, -0.12425698102638041, -0.08094531518938053]
+    t_cam0 = [0.0,0.0,0.0]
 
 
 
@@ -230,17 +287,25 @@ if __name__=="__main__" :
 
     t_cam1= [-0.05880865244539242, -0.1215735034443526, -0.0856024019820022]
 
-    q1 = Quaternion(rotMat_cam0)
-    q2 = Quaternion(rotMat_cam1)
-
-    print(q1.to_rvec())
-    print(q2.to_rvec())
-
+    #q1 = Quaternion(rotMat_cam0)
+    rot_mat = Rotations([180,0,45], extrinsic=True).rotation_matrix
+    q1 = Quaternion( [-1,0.0,0.0,0.0])
+    q2 = Quaternion([0.0, 0.18334911693105377, 0.006365956667309573], deg=True) 
+    q3 = Quaternion([0.0, 30.0, 0.0], deg=True)
+    print(q2)
+    print(q1.inv().rotate(q1.rotate([0,0,0.5]).to_list()[:3]).to_list())#.to_euler(deg=True))
+    #print(q2.to_rvec())
+    print((q1*q3).rotate([0,0,-1]).to_list())
 
     fig, ax = plt.subplots(1)
-    plot_3d(ax, [q1,q2], [t_cam0, t_cam1])
-    plt.show(fig)
+    #plot_3d(ax, [q1.rotate([1,1,1]), (q1.inv()).rotate([1,1,1])], [[0.0,0.50,0], [0.0,0.0,0.5]])
+    plot_3d(ax, (q1*q3).rotate([0,0,-1]))
+    plt.show()
     
-
+"""
+[[ 6.71487041e-01,  7.41014515e-01,  1.62525572e-03],
+ [-7.41009773e-01,  6.71488718e-01, -2.72375351e-03],
+ [-3.10968177e-03,  6.24634817e-04,  9.99994970e-01]]
+"""
 
 
